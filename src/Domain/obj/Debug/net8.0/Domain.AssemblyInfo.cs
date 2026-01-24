c42c881d4e909027ed9dc6c16ea4cb365d64200b
William Bisimwa <bisimwawilliam@icloud.com>
2026-01-24 18:20:18 +0200

    remove the test console app

--- src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5b9493b66666b635de16aa52a136f35d23f9b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+563fa959e4f563fa45944172960509118b078207")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
